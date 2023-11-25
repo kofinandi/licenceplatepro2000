b2c71530450a37ad09b5504a9d4e98acf4889b47
Nándor Kőfaragó <kofarago.nandi@gmail.com>
2023-11-25 18:14:26 +0100

    Robust convolution

--- convolutional_ocr.py
+++ convolutional_ocr.py
@@ -3,13 +3,23 @@ import numpy as np
 import os
 
 class ConvolutionalOCR:
-    def __init__(self):
+    def __init__(self, conv_threshold=0.65, nms_threshold=0.1, padding=0.2, plate_size=(40, 205), character_size=(0.6, 0.9), character_size_step=0.05, angle_range=6, angle_step=3):
         # create a list of all the template images
         self.template_imgs = {}
-        for img_file in os.listdir('font_img'):
-            self.template_imgs[img_file[0]] = cv2.imread('font_img/' + img_file, 0)
+        for img_file in os.listdir('font_img_regi'):
+            self.template_imgs[img_file[0]] = cv2.imread('font_img_regi/' + img_file, 0)
 
-    def _apply_nms(self, boxes, threshold=0.5):
+        self.conv_threshold = conv_threshold
+        self.nms_threshold = nms_threshold
+        self.padding = padding
+        self.plate_size = plate_size
+
+        self.character_size = character_size
+        self.character_size_step = character_size_step
+        self.angle_range = angle_range
+        self.angle_step = angle_step
+
+    def _apply_nms(self, boxes):
         # Sort boxes by their confidence score in descending order
         boxes.sort(key=lambda x: x[0], reverse=True)
 
@@ -43,7 +53,7 @@ class ConvolutionalOCR:
                 overlap = intersection_area / float(selected_area + box_area - intersection_area)
 
                 # If overlap is less than the threshold, keep the box
-                if overlap <= threshold:
+                if overlap <= self.nms_threshold:
                     remaining_boxes.append(box)
 
             boxes = remaining_boxes
@@ -59,52 +69,112 @@ class ConvolutionalOCR:
             cv2.rectangle(image_with_boxes, (x1, y1), (x2, y2), (0, 0, 0), 2)
         return image_with_boxes
     
+    def _rotate_image(self, image, angle):
+        image = cv2.copyMakeBorder(image, 50, 50, 50, 50, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+        height, width = image.shape[:2]
+        image_center = (width / 2, height / 2)
+        rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+        rotated_image = cv2.warpAffine(image, rotation_mat, (width, height))
+        rotated_image = rotated_image[50:-50, 50:-50]
+        return rotated_image
+    
     def detect(self, plate_img, draw_boxes=False):
         # Convert images to grayscale
         plate_gray = cv2.cvtColor(plate_img, cv2.COLOR_BGR2GRAY)
 
         # resize the image
         height, width = plate_gray.shape
-        plate_gray = cv2.resize(plate_gray, (int(height / 40 * 205), height))
+        plate_gray = cv2.resize(plate_gray, (int(height / self.plate_size[0] * self.plate_size[1]), height))
 
-        r = 0.2
+        p = int(height * self.padding)
 
-        # pad the plate image with extra white pixels to avoid errors
-        plate_gray = cv2.copyMakeBorder(plate_gray, int(height * r), int(height * r), int(width * r), int(width * r), cv2.BORDER_CONSTANT, value=[255, 255, 255])
+        # pad the plate image with the border pixels
+        plate_gray = cv2.copyMakeBorder(plate_gray, p, p, p, p, cv2.BORDER_CONSTANT, value=[255, 255, 255])
 
         detections = []
         for key, template in self.template_imgs.items():
-            character_ratio = 0.7
+            for character_ratio in np.linspace(self.character_size[0], self.character_size[1], num=int((self.character_size[1]-self.character_size[0])/self.character_size_step)+1):
+                for angle in range(-self.angle_range, self.angle_range+1, self.angle_step):
+                    template = self._rotate_image(template, angle)
+                    template_height, template_width = template.shape
+                    template = cv2.resize(template, (int(height * character_ratio * template_width / template_height), int(height * character_ratio)))
+                    match = cv2.matchTemplate(plate_gray, template, cv2.TM_CCOEFF_NORMED)
 
-            template_height, template_width = template.shape
-            template = cv2.resize(template, (int(height * character_ratio * template_width / template_height), int(height * character_ratio)))
-
-            match = cv2.matchTemplate(plate_gray, template, cv2.TM_CCOEFF_NORMED)
-
-            # print(key + ': ' + str(np.max(match)))
-
-            # Set a threshold to identify matches
-            threshold = 0.7
-            locations = np.where(match >= threshold)
-            # locations is a tuple of arrays, so we need to convert it to a n x 3 array format adding the third dimension as the value of the match
-            locations = list(zip(*locations[::-1]))
-            locations = [list([match[elem[1], elem[0]]]) + list(elem) + list(template.shape) + list(key) for elem in locations]
-            detections.extend(locations)
+                    locations = np.where(match >= self.conv_threshold)
+                    # locations is a tuple of arrays, so we need to convert it to a n x 3 array format adding the third dimension as the value of the match
+                    locations = list(zip(*locations[::-1]))
+                    locations = [list([match[elem[1], elem[0]]]) + list(elem) + list(template.shape) + list(key) for elem in locations]
+                    detections.extend(locations)
 
         # Apply NMS
-        picked_boxes = self._apply_nms(detections, threshold=0.1)
-
-        # Draw bounding boxes around detected positions after NMS
-        if draw_boxes:
-            plate_with_boxes = self._draw_boxes(plate_gray, picked_boxes)
-            cv2.imshow('Detected Characters', plate_with_boxes)
-            #cv2.waitKey(0)
-            #cv2.destroyAllWindows()
+        picked_boxes = self._apply_nms(detections)
 
         picked_boxes.sort(key=lambda x: x[1])
         # print the detected text
         text = ''
         for box in picked_boxes:
             text += box[5]
+
+        text = text.upper()
+
+        # Draw bounding boxes around detected positions after NMS
+        if draw_boxes:
+            plate_with_boxes = self._draw_boxes(plate_gray, picked_boxes)
+            cv2.imshow(text, plate_with_boxes)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+
         # capitalize the text
-        return text.upper()
+        return text
+    
+    def parse(self, text):
+        if len(text) == 7:
+            text = text[:4] + text[4:].replace('O', '0')
+            text = text[:4] + text[4:].replace('D', '0')
+            text = text[:4] + text[4:].replace('I', '1')
+            text = text[:4] + text[4:].replace('Q', '0')
+            text = text[:4] + text[4:].replace('Z', '2')
+            text = text[:4] + text[4:].replace('S', '5')
+            text = text[:4] + text[4:].replace('B', '8')
+            text = text[:4] + text[4:].replace('G', '6')
+            text = text[:4] + text[4:].replace('T', '7')
+            text = text[:4] + text[4:].replace('A', '4')
+
+            text = text[:4].replace('0', 'O') + text[4:]
+            text = text[:4].replace('1', 'I') + text[4:]
+            text = text[:4].replace('2', 'Z') + text[4:]
+            text = text[:4].replace('5', 'S') + text[4:]
+            text = text[:4].replace('8', 'B') + text[4:]
+            text = text[:4].replace('6', 'G') + text[4:]
+            text = text[:4].replace('7', 'T') + text[4:]
+            text = text[:4].replace('4', 'A') + text[4:]
+
+            # format the text from AAAA111 to AA AA-111
+            text = text[:2] + ' ' + text[2:4] + '-' + text[4:]
+
+        if len(text) == 6:
+            text = text[:3] + text[3:].replace('O', '0')
+            text = text[:3] + text[3:].replace('D', '0')
+            text = text[:3] + text[3:].replace('I', '1')
+            text = text[:3] + text[3:].replace('Q', '0')
+            text = text[:3] + text[3:].replace('Z', '2')
+            text = text[:3] + text[3:].replace('S', '5')
+            text = text[:3] + text[3:].replace('B', '8')
+            text = text[:3] + text[3:].replace('G', '6')
+            text = text[:3] + text[3:].replace('T', '7')
+            text = text[:3] + text[3:].replace('A', '4')
+
+            text = text[:3].replace('0', 'O') + text[3:]
+            text = text[:3].replace('1', 'I') + text[3:]
+            text = text[:3].replace('2', 'Z') + text[3:]
+            text = text[:3].replace('5', 'S') + text[3:]
+            text = text[:3].replace('8', 'B') + text[3:]
+            text = text[:3].replace('6', 'G') + text[3:]
+            text = text[:3].replace('7', 'T') + text[3:]
+            text = text[:3].replace('4', 'A') + text[3:]
+
+            # format the text from AAA111 to AAA-111
+            text = text[:3] + '-' + text[3:]
+
+        return text
